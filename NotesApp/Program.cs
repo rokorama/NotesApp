5506fc7b2759b08574cb24743715ee19bf7ead7b
Rokas <rokamanauskas@gmail.com>
2022-07-06 19:10:07 +0300

    Add note manipulation services to the app builder

--- NotesApp/Program.cs
+++ NotesApp/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+using NotesApp.BusinessLogic;
 using NotesApp.DataAccess;
+using NotesAppBusinessLogic;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 string connString = builder.Configuration.GetConnectionString("Database");
 builder.Services.AddDbContext<ApplicationDbContext>(opt => opt.UseSqlServer(connString));
+builder.Services.AddScoped<INoteRepository, NoteRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<INoteService, NoteService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
